a59e03f0617044b9cee948614e7ee96ae832a951
Kirilin Artem <kirilinartem@yandex.ru>
2025-10-26 23:02:46 +0700

    Первичная настройка и проверка базы

---
+++ app/database/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy import func
+from config import settings
+from datetime import datetime
+
+DATABASE_URL = settings.getAsyncDbUrl()
+
+engine = create_async_engine(url=DATABASE_URL)
+asyncSessionMaker = async_sessionmaker(engine,expire_on_commit=False)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_ay: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower() + 's'
--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from app.database.database import asyncSessionMaker
+from sqlalchemy import text
 
 app = FastAPI()
 
 @app.get("/")
-def rootApi():
-    return {'message': 'pass'}
+async def rootApi():
+    async with asyncSessionMaker() as session:
+        dbRes = await session.execute(text('SELECT 1'))
+        dbRes = dbRes.scalar()
+    return {'message': 'pass', 'db': dbRes}
