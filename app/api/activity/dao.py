243413922bb3777e1463fb0ffb7cd743f549641e
Kirilin Artem <kirilinartem@yandex.ru>
2025-11-01 01:17:06 +0700

    Поиск семейства активностей

--- app/api/activity/dao.py
+++ app/api/activity/dao.py
@@ -1,30 +1,55 @@
 from app.database.dao import BaseDAO
-from app.database.models import Organization
+from app.database.models import Activity
 from app.database.database import asyncSessionMaker
 from sqlalchemy.future import select
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import aliased
+from sqlalchemy.sql.expression import literal
 
-class OrganizationDAO(BaseDAO):
-    model = Organization
+class ActivityDAO(BaseDAO):
+    model = Activity
 
     @classmethod
-    async def findFullData(cls, id: int) -> dict | None:
+    async def getItAndAllDescendansIdCte(cls, id: int, maxDepth: int = 3):
+        """
+        Генератор запроса: Активность по id и все её потомки до maxDepth уровня
+        """
+        # Рекурсивный CTE для получения всех дочерних активностей
+        ActivityHierarchy = aliased(Activity, name="activity_hierarchy")
+        
+        # Базовый случай - начальная активность
+        base_case = select(
+            ActivityHierarchy.id,
+            ActivityHierarchy.parent_id,
+            literal(0).label('level')  # Уровень вложенности
+        ).where(ActivityHierarchy.id == id).cte(
+            name="activity_tree", 
+            recursive=True
+        )
+        
+        # Рекурсивный случай - дочерние активности
+        recursive_case = select(
+            ActivityHierarchy.id,
+            ActivityHierarchy.parent_id,
+            (base_case.c.level + 1).label('level')
+        ).join(
+                base_case, 
+                ActivityHierarchy.parent_id == base_case.c.id
+        ).where(
+            base_case.c.level < maxDepth - 1  # Ограничение глубины
+        )
+        
+        # Объединяем базовый и рекурсивный случаи
+        activity_tree_cte = base_case.union_all(recursive_case)
+
+        return activity_tree_cte
+    
+    @classmethod
+    async def getItAndAllDescendans(cls, id: int, maxDepth: int = 3):
+        cte = await cls.getItAndAllDescendansIdCte(id, maxDepth)
+        query = select(Activity).join(cte,cte.c.id == Activity.id)
+
         async with asyncSessionMaker() as session:
-            # Запрос для получения информации о студенте вместе с информацией о факультете
-            query = (
-                select(cls.model)
-                .options(joinedload(cls.model.phones),joinedload(cls.model.building),joinedload(cls.model.activities))
-                .filter_by(id=id)
-            )
             result = await session.execute(query)
-            result = result.unique().scalar_one_or_none()
+            result = result.scalars().all()
 
-            # Если студент не найден, возвращаем None
-            if not result:
-                return None
-
-            dictResult = result.toDict()
-            dictResult['phones'] = [phone.toDict() for phone in result.phones]
-            dictResult['building'] = result.building.toDict()
-            dictResult['activities'] = [activity.toDict() for activity in result.activities]
-            return dictResult
+        return result
