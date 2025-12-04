74c4b8993867925dcb0e4a8d9bc1371f027578ac
Kirilin Artem <kirilinartem@yandex.ru>
2025-12-04 15:00:09 +0700

    Поиск зданий в зоне (в круге и в прямоугольнике)

--- app/api/building/dao.py
+++ app/api/building/dao.py
@@ -4,6 +4,7 @@ from app.database.models import Building
 from app.database.database import asyncSessionMaker
 from sqlalchemy.future import select
 from sqlalchemy.orm import joinedload
+from sqlalchemy import func
 from app.api.building.models import InCircle, InBox, isValidation
 
 class BuildingDAO(BaseDAO):
@@ -15,11 +16,49 @@ class BuildingDAO(BaseDAO):
     
     @classmethod
     async def findAllInCircle(cls, session: AsyncSession, **inCircle):
-        print('in circle')
-        return []
+        lng = inCircle['longitude']
+        lat = inCircle['latitude']
+        radiusKm = inCircle['radius']
+        point = f'POINT({lng} {lat})'
+        
+        query = (
+            select(cls.model)
+            .filter(
+                func.ST_DWithin(
+                    Building.geom,
+                    func.ST_GeogFromText(point),
+                    radiusKm * 1000  # конвертируем км в метры
+                )
+            )
+        )
+        result = await session.execute(query)
+        result = result.scalars().all()
+
+        return result
+
 
     @classmethod
     async def findAllInBox(cls, session: AsyncSession, **inBox):
-        print('in box')
-        return []
+        lng = inBox['longitude']
+        lat = inBox['latitude']
+        bboxLng = inBox['bbox_longitude']
+        bboxLat = inBox['bbox_latitude']
 
+        minLng = lng - bboxLng / 2
+        maxLng = lng + bboxLng / 2
+        minLat = lat - bboxLat / 2
+        maxLat = lat + bboxLat / 2
+        bbox = f'POLYGON(({minLng} {minLat}, {minLng} {maxLat}, {maxLng} {maxLat}, {maxLng} {minLat}, {minLng} {minLat}))'
+        query = (
+            select(cls.model)
+            .filter(
+                func.ST_Intersects(
+                    Building.geom,
+                    func.ST_GeogFromText(bbox)
+                )
+            )
+        )
+        result = await session.execute(query)
+        result = result.scalars().all()
+
+        return result
